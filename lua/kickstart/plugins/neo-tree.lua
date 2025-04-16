9b168f32b6d66d8369836407311bd7f85fdbe1fc
kalex3 <31569459+kalex3@users.noreply.github.com>
2025-04-16 23:46:01 +0200

    added leader+n keyboard shortcut

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { "<leader>n", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
   },
   opts = {
     filesystem = {
